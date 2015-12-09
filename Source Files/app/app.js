6e51474d70a8b62c6718a763515fdaddccf022eb
baothi <baothi246@gmail.com>
2015-12-09 16:23:45 +0700

    hoc toi bai 12

--- Source Files/app/app.js
+++ Source Files/app/app.js
@@ -1,19 +1,14 @@
 var myapp = angular.module("myapp",[]);
-// myapp.controller("ShowMessage", function($scope){
-//   // $scope.message ="xin chào";
-//   // $scope.title= 'Tieu de';
-//   $scope.data = {};
-//   $scope.data.message = "noi dung";
-// });
-// myapp.controller("controller1", function($scope){
-//   $scope.message = 'noi dung 1';
-// });
+// phần 1
+myapp.controller("controller1", function($scope){
+  $scope.message = 'noi dung 1';
+});
 
-// myapp.controller("controller2", function($scope){
-//   $scope.message = 'noi dung 2';
-// });
+myapp.controller("controller2", function($scope){
+  $scope.message = 'noi dung 2';
+});
 
-
+// phần 2
 myapp.controller("controller1", function($scope, $rootScope){
   // $scope.message = 'noi dung chung';
   // $rootScope.messages = "noi dung da thay doi"
@@ -22,10 +17,10 @@ myapp.controller("controller1", function($scope, $rootScope){
 myapp.controller("controller2", function($scope){
   // $scope.message = 'noi dung 2';
 });
+
+// phần 4
 myapp.controller("maytinh",function($scope){
   $scope.ketqua = "Kết Quả Là: ";
-
-
 
   $scope.tinh = function(){
     var so1 = parseInt($scope.so1);
@@ -42,6 +37,56 @@ myapp.controller("maytinh",function($scope){
     }
     else {
       $scope.ketqua = 'Kết Quả Là: ' + (so1 / so2);
+    }
+  };
+});
+
+myapp.directive("ngFormlogin", function(){
+  return {
+    templateUrl: 'app/templates/login_form.html'
+  };
+});
+
+myapp.controller('loginController', function($scope){
+  $scope.checkLogin = function(){
+    var username = $scope.username;
+    var password = $scope.password;
+
+    alert("Username: " + username + " -Password: " + password);
+  };
+});
+// phần 5
+myapp.controller("MessageController", function($scope){
+  $scope.ShowMessage = function(){
+    alert("MessageController !");
+  }
+});
+
+myapp.directive('message',function(){
+  return function(scope, element, attrs){
+    element.bind('mouseenter', function(){
+      scope.ShowMessage();//cach 1
+      scope.$apply("ShowMessage()"); // cach 2
+    });
+  };
+});
+
+// phần 6 Directive Linking
+myapp.controller("MessageController1", function($scope){
+
+  $scope.ShowMessage = function(){
+    alert("MessageController1")
+  };
+});
+
+myapp.directive('message',function(){
+  return {
+    restrict : "E",
+    link : function(scope, element, attrs){
+      // scope.ShowMessage();
+      element.bind("mouseenter", function(){
+        scope.ShowMessage();
+      });
     }
   };
 });
